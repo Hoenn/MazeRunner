42fca6b31322de874edbd1dc24fcb98e8b677f71
Hoenn <skatevan1313@gmail.com>
2015-06-03 11:23:35 -0400

    Removed unused Midi code

--- src/mazerunner/Resources.java
+++ src/mazerunner/Resources.java
@@ -1,20 +1,7 @@
 package mazerunner;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequencer;
-import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Track;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
@@ -36,46 +23,7 @@ public class Resources
 		sounds.put("spawnArcade", loadSound("res/spawn.wav"));
 		sounds.put("spawnZen", loadSound("res/spawnZen.wav"));
 		sounds.put("spawnMaze", loadSound("res/spawnMaze.wav"));
-		Sequencer sequencer =null;
-		try
-		{
-			sequencer = MidiSystem.getSequencer();
-		} catch (MidiUnavailableException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try
-		{
-			sequencer.open();
-		} catch (MidiUnavailableException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		InputStream is =null;
-		try
-		{
-			is = new BufferedInputStream(new FileInputStream(new File("res/DuckTales_the_moon.mid")));
-		} catch (FileNotFoundException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try
-		{
-			sequencer.setSequence(is);
-		} catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidMidiDataException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//StartsPlayingMusic 
-		//sequencer.start();
+
 		colors = new HashMap<String, Color>();
 		colors.put("myRed", new Color(217, 27, 12));
 		colors.put("myBlue", new Color(15, 57, 224));
