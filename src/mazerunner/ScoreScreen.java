ceb858888090ff55fe3e857fb738eacf54f38f95
Hoenn <skatevan1313@gmail.com>
2015-06-06 13:19:46 -0400

    removed unused imports, unused fonts

--- src/mazerunner/ScoreScreen.java
+++ src/mazerunner/ScoreScreen.java
@@ -1,14 +1,10 @@
 package mazerunner;
 
-import java.awt.Font;
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
@@ -17,12 +13,7 @@ import org.newdawn.slick.state.transition.FadeOutTransition;
 public class ScoreScreen extends BasicGameState
 {
 	private final int id;
-	
-	private float mapX;
-
-	private Font font;
-	private TrueTypeFont trueTypeFont;
-	
+		
 	private String highScores_Arcade;
 	private String dates_Arcade;
 	private String highScores_Zen;
@@ -40,8 +31,7 @@ public class ScoreScreen extends BasicGameState
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException
 	{
-		font = new Font("monospace", Font.BOLD, 35);
-        trueTypeFont = new TrueTypeFont(font, false);
+
 	}
 	
 
